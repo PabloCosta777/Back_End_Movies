1fcbf6c470f242ae101b8d6478ad4d56e273b541
Pablo costa <snoop.gz@gmail.com>
2022-05-23 17:53:13 -0300

    Update relations

--- Api/Models/relations.js
+++ Api/Models/relations.js
@@ -2,11 +2,9 @@ const character  =require('./character')
 const movie      =require('./movie')
 const genre      =require('./genre')
 
-function init(){
-    character.belongsToMany(movie, { through: "character_movies" });
-    movie.belongsToMany(character, { through: "character_movies" });
+character.belongsToMany(movie, { through: "character_movies" });
+movie.belongsToMany(character, { through: "character_movies" });
 
-    movie.belongsToMany(genre, { through: "movie_genre" });
-    genre.belongsToMany(movie, { through: "movie_genre" });
-}
-init()
+movie.belongsToMany(genre, { through: "movie_genre" });
+genre.belongsToMany(movie, { through: "movie_genre" });
+
