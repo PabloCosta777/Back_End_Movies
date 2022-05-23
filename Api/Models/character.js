b43c2d4b4d1e3a9757b5a48a7e15ab5ddc1a5cd7
Pablo costa <snoop.gz@gmail.com>
2022-05-23 17:02:05 -0300

    character model created

---
+++ Api/Models/character.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require("sequelize");
+const conn =require('../db')
+
+const character = conn.define("character", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  age: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  weight: {
+    type: DataTypes.DECIMAL,
+    allowNull: false
+  },
+  history: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  }
+});
+module.exports = character 
